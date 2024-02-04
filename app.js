209a192d3cf041778080b05747b0d355eef3b2dd
daniyal ali <daniyalali12568@gmail.com>
2024-02-04 19:57:16 +0500

    cors

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(router)
 
 cloudinary.config({
