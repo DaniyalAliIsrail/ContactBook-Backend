8c8571cd375976c12fee49af55a41222455a11ea
daniyal ali <daniyalali12568@gmail.com>
2024-02-05 00:51:36 +0500

    cors

--- controller/userController.js
+++ controller/userController.js
@@ -13,9 +13,6 @@ const dashboardValidate = async (req, res) => {
 };
 
 const postController = async (req, res) => {
-
-  res.json({msg:"ok"});
-  return
   try {
     const image = req.files[0].path;
 
