2e3f3bebfac1876ee7b2d9f65b7d10b0ce48ef72
daniyal ali <daniyalali12568@gmail.com>
2024-02-04 23:09:18 +0500

    cors-chnages

--- app.js
+++ app.js
@@ -9,11 +9,7 @@ env.config()
 const PORT = process.env.PORT || 8000;
 const app = express();
 
-app.use(cors({
-  origin: '*',
-  origin:'http://localhost:5173',
-  credentials: true,
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(router)
